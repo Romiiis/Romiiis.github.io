fa5b4580d807475488eafdcfde92f3f4febc7649
Roman Pejs <romispejs@gmail.com>
2024-05-22 20:58:18 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {IntroComponent} from "./intro/intro.component";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, IntroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MyWebsite';
+  title = 'MyWeb';
 }
